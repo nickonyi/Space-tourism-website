0521e5e09281de4707b0612db1d29d5e43b4d738
nickonyi <nickoluoch1130@gmail.com>
2022-11-13 05:27:11 +0300

    Space tourism website

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,10 @@
-const swup = new Swup();
+const nav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+
+//when someone clicks my hamburger button
+navToggle.addEventListener("click", () => {
+    //if the nav is open close it
+    const blindNigga = nav.getAttribute("data-visible");
+    console.log(blindNigga);
+    //if the nav is closed open it
+});
